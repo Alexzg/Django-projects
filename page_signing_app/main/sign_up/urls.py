31ceda1682726e047b3ff5fee041192fd80eaea1
alexzg <azlex0@gmail.com>
2018-09-24 15:32:43 +0300

    Signup from form to model
    The imported data from the form(HTML)
    are saved to the db using the custom made model "User"

--- page_signing_app/main/sign_up/urls.py
+++ page_signing_app/main/sign_up/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "sign_up"
 urlpatterns = [
+    path('', views.signup, name='sign_up_form'),
     path('signup/', views.signup, name='sign_up_form'),
     path('loggedin/', views.loggedin, name='loggedin')
 ]
--- page_signing_app/main/sign_up/views.py
+++ page_signing_app/main/sign_up/views.py
@@ -1,22 +1,44 @@
 from django.shortcuts import render, redirect
 
-from django.http import HttpResponseRedirect
-from .forms import SignupForm
+from django.http import HttpRequest, HttpResponseRedirect
 
-
-# def signup(request):
-#     path = "sign_up/sign_up_form.html"
-#     return render(request, path)
+from .models import User
 
 def signup(request):
     path = "sign_up/sign_up_form.html"
     if request.method == 'POST':
-            form = SignupForm(request.POST)
-            if form.is_valid():
-                return HttpResponseRedirect('/loggedin/')
+        form = request.POST # Load the data from request to "form"
+        firstname = form.get('firstname', default='No such field')
+        lastname = form.get('lastname', default='No such field')
+        email = form.get('email', default='No such field')
+        pw = form.get('password', default='No such field')
+
+        saved_users = User.objects.filter(name=firstname)
+        try:
+            User.objects.get(name=firstname) # Raises error if doesn't exist
+            print("Already exist", saved_users)
+        except: # Save new "user" in "model"
+            User(
+            name=firstname, surname=lastname,
+            email=email, password=pw).save()
+            print("New entry")
+            return HttpResponseRedirect('/loggedin/')
     else:
-        form = SignupForm()
-    return render(request, path, {'form':form})
+        pass
+    return render(request, path)
+
+# https://docs.djangoproject.com/en/2.1/topics/auth/default/#how-to-log-a-user-in
+# def login(request):
+#     path = "sign_up/sign_up_form.html" # NEED TO CHAGNE
+#     username = request.POST['username']
+#     password = request.POST['password']
+#     user = authenticate(request, username=username, password=password)
+#     if user is not None:
+#         login(request, user)
+#         # Redirect to page
+#     else:
+#         # Return an 'invalid login' message
+#     return render(request, path)
 
 def loggedin(request):
     path = "sign_up/logged_in.html"
